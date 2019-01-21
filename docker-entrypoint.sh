7091075e6532e1d88f2048d4c5998235648a13bb
xwp <xwp_fullstack@163.com>
2019-01-21 10:18:31 +0800

    修改

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,2 @@
-export FLASK_APP=/code/show_image/__init__.py
-flask initdb
-gunicorn -w 4 -b 0.0.0.0:6666 show_image:app
+export FLASK_APP=/code/show/App.py
+gunicorn -w 4 -b 0.0.0.0:6666 App:app
--- to_show.py
+++ to_show.py
@@ -1,18 +1,18 @@
 # -*- coding:utf-8 -*-
 import time
 from flask import request
-
 from splinter import Browser
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-
+from flask import Flask
+from config import Config
+app = Flask(__name__)
+# 配置
+app.config.from_object(Config)
 browser = Browser('chrome')
 browser.driver.maximize_window()
-
 wait = WebDriverWait(browser.driver, 60)
-
-from show_image import app
 @app.route('/showimg', methods=['GET', 'POST'])
 def showimg():
     # 获取到图片url
@@ -29,7 +29,12 @@ def showimg():
 
     # from tasks import _toShow
     # status = _toShow.delay(img_path)
-    status = _toShow(img_path)
+    for i in range(3):
+        status = _toShow(img_path)
+        if status[0] == 200:
+            break
+        else:
+            continue
 
     return status
 
@@ -41,11 +46,11 @@ def _toShow(img_path):
     for i in range(5):
         img = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'img')))
         if img:
-            code = 222
+            code = 200
             msg = 'ok'
             break
         else:
-            code = 111
+            code = 500
             msg = 'null'
             time.sleep(0.5)
             continue
@@ -55,4 +60,4 @@ def _toShow(img_path):
     return status
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0',port=5005)
